87dcc444a3bef8f335a5a1d0ad01f8a5277f9321
Tor-Einar Skog <tor-einar.skog@nibio.no>
2023-02-22 09:32:29 +0100

    Translations done and converted (2023-02-22)

--- i18n/csv_to_properties.py
+++ i18n/csv_to_properties.py
@@ -60,8 +60,8 @@ for file_name in files:
         path_elements = base_file_name.split("/")
         base_file_name = path_elements[len(path_elements)-1]
         path = "/".join(path_elements[0:len(path_elements)-1])
-        #print (path)
-        #print(base_file_name)
+        print (path)
+        print(base_file_name)
         props_all = {}
         for lang in rowreader.fieldnames[1:]:
             props_for_lang = {}
